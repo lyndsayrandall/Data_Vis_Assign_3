518cdad5e8c90b345b13c57842783bb82a7027bc
Mark Randall <lyndsayrandall@me.com>
2025-06-06 19:52:29 +1000

    About Modal 99% done could add my business colours hist and dense done overflow seems sorted.

--- s9001731_Assign_3/modalsDispCode.R
+++ s9001731_Assign_3/modalsDispCode.R
@@ -14,7 +14,9 @@ observeEvent(input[[value]], {
                 " ",
                 "Perry Swift. (2025). Potato Alert, A statistics companion app for World Of Warships. razaqq. 
                     Retrieved 10 May from https://github.com/razaqq/PotatoAlert?tab=readme-ov-file",
-                " ",
+                "Wikipedia. (2025, 25 April 2025). World of Warships. Wikipedia Foundation. 
+                Retrieved 10 May from https://en.wikipedia.org/wiki/World_of_Warships",
+               " ",
           sep = "<br>")),
     footer = tagList(modalButton("Return"))
   ))})    
@@ -50,21 +52,21 @@ dispAboutModal <- function(input,value) {
                      width = "100%"
                  ))),
         column(11,
-               HTML(paste0("World of Warships is a 'free' multiplayer on line game developed by Wargaming. 
+               HTML(paste0("&#8195;World of Warships &#169; is a 'free' multiplayer on line game developed by Wargaming. 
                The game was annouced as World of Battleships to complement two other games in the wargaming stable. 
                This annoucement occured 16 August 2011. The game conducted a closed beta testing 12 March 2015 
                with a name change of World of Warships(2012). Open beta testing commenced 2 July 2015. 
                The microsoft windows official launch was 17 September 2015 and in the Microsoft Store and Steam 
-               15 November 2015. ",
-               "The Game genre is considered massively multiplayer online(MMO)/Vehicular Combat Game/
+               15 November 2015.<br> ",
+               "&#8195;The Game genre is considered massively multiplayer online(MMO)/Vehicular Combat Game/
                third person shooter(TPS). It has games clients for the following
                platforms Microsoft Windows, macOS (discontinued), iOS, Android, PlayStation 4, PlayStation 5, 
-               Xbox One, Xbox Series X/S. ",
-               "'Players control warships of choice and can battle other random players on the server, 
+               Xbox One, Xbox Series X/S.<br> ",
+               "'&#8195;Players control warships of choice and can battle other random players on the server, 
                play cooperative battles against bots, or participate in an advanced player versus environment 
-               (PvE) battle mode. For the most skilled players, two seasonal competitive modes are also available.'",
-                          " ",
-                          sep = "<br>")),
+               (PvE) battle mode. For the most skilled players, two seasonal competitive modes are also available.'<br>",
+               "(Wikipedia.2025, 25 April 2025)",
+               sep = "<br>")),
       )),
       fluidRow(
         column(1, 
@@ -75,19 +77,32 @@ dispAboutModal <- function(input,value) {
                              width = "100%"
                     ))),
         column(11,
-               HTML(paste("Beadell, S. J. L. R. N. (1941). View from HMS HERMIONE of HMS LEGION moving alongside the damaged and 
-                  listing HMS ARK ROYAL in order to take off survivors. In HMS_Ark_Royal_sinking_2.jpg (Ed.), jpg (Vol. 761x 
-                  571px). Imperial War Museums(collection no. 4700-01): Imperial War Museums.",
-                          " ",
-                          "Wargaming.net. (2012-2025, 2025). World of Warships. Wargaming.net. 
-                    Retrieved 10 May from https://worldofwarships.asia/en/content/game/",
-                          " ",
-                          "Perry Swift. (2025). Potato Alert, A statistics companion app for World Of Warships. razaqq. 
-                    Retrieved 10 May from https://github.com/razaqq/PotatoAlert?tab=readme-ov-file",
-                          " ",
-                          sep = "<br>")),
+             
+               HTML(paste0("&#8195;The Potato Alert programme was developed by Perry Swift in c++.  It is as GUI
+                          that collects data from the World of Warships through application
+                          programming interfaces(API).<br>",
+                          
+                          "&#8195The programme can be downloaded from the git hub site;  
+                          <a class = 'add_link' href='https://github.com/razaqq/PotatoAlert'>link  </a> or 
+                          installed via some other programmes that manage World of Warships sanctioned 
+                          modifications. The preferred installation is via the git hub location as it stores the 
+                          application in a different file and conducts automatic update searches.<br>",
+                          
+                          "&#8195;The game also stores to computer in:<br>",
+                               "<ul>
+                               <li>&#8195;C:/Users/&#60;USER&#62;/AppData/Local/PotatoAlert/Matches 
+                                             a match csv file and a match history database created by SQLite.</li>
+                               <li>&#8195;C:/&#60;USER&#62;/Mark/AppData/Local/PotatoAlert/PotatoAlert.log a file to 
+                          enable error locations.</li>
+                          </ul>",
+                          "(Perry Swift,2025),br>",
+                          sep = "<br>"
+                          
+                          
+                          
+                          )),
                )),
       footer = tagList(modalButton("Return")),
          ))})  
 }#EOFnAbout
-  
+ 
